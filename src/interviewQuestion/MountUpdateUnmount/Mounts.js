13b9904851bc6472ef28b8ef1576aae80eca918a
Devansh <dev048673@gmail.com>
2023-02-24 22:46:30 +0530

    MONUNTING, Toggle by useState

---
+++ src/interviewQuestion/MountUpdateUnmount/Mounts.js
@@ -0,0 +1,21 @@
+// compount did mount
+
+useEffect(() => {
+
+})
+
+//component update
+useEffect(() => {
+
+}, [user])
+
+//unmounting
+
+useEffect(() => {
+
+    return (() => {
+
+    })
+}, [])
+
+//=(https://www.youtube.com/watch?v=RapcagVtB-0&t=515s&ab_channel=Dev.Aditya)
